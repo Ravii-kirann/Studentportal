81d93c0e951ac5917de688e2f74dc33a7496832c
joseph.prasadula <joseph.prasadula@aerosimple.in>
2024-04-20 23:41:10 +0530

    front bus ticket part

--- client/src/pages/BusTicket.jsx
+++ client/src/pages/BusTicket.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from '../components/header'
 
 export default function BusTicket() {
+    const [busTicketNumber, setBusTicketNumber] = useState(0);
+
+    const [zoneTickets, setZoneTickets] = useState({zone1: '0', zone2: '0', zone3: '0'})
+
+    const purchaseBusTicket = () => {
+        fetch(`http://localhost:1337/api/busTicket/bus-cards`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({quantity: busTicketNumber})
+            }).then(data => {
+                let result;
+                console.log('data', data);
+                result = data.json();
+                return result
+              }).then(result => {
+                console.log('result', result)
+                if (result === 'Signout success!') {
+                   
+                } else {
+                console.log('result not ok', result)
+                alert('Something went wrong! try again after refresh');
+                } 
+              }).catch(err => {
+                console.log('err', err);
+                alert('Something went wrong! try again after refresh');
+              })
+    };
+    
   return (
     <>
     <Header />
@@ -10,18 +40,18 @@ export default function BusTicket() {
         <div class="tickets">
             <div class="ticket">
                 <h2>Zone 1</h2>
-                <p>$2 per ticket</p>
-                <input type="number" id="zone1" min="0" max="10" value="0" />
+                <p>$2 per ticket</p>    
+                <input type="number" id="zone1" min="0" value={zoneTickets?.zone1} onChange={(e) => {setZoneTickets(prev => ({...prev, zone1: e.target?.value}))}}/>
             </div>
             <div class="ticket">
                 <h2>Zone 2</h2>
                 <p>$4 per ticket</p>
-                <input type="number" id="zone2" min="0" max="10" value="0" />
+                <input type="number" id="zone2" min="0" value={zoneTickets?.zone2} onChange={(e) => {setZoneTickets(prev => ({...prev, zone2: e.target?.value}))}}/>
             </div>
             <div class="ticket">
                 <h2>Zone 3</h2>
                 <p>$6 per ticket</p>
-                <input type="number" id="zone3" min="0" max="10" value="0" />
+                <input type="number" id="zone3" min="0" value={zoneTickets?.zone3} onChange={(e) => {setZoneTickets(prev => ({...prev, zone3: e.target?.value}))}}/>
             </div>
         </div>
         <button onclick="purchaseTickets()">Purchase Tickets</button>
@@ -29,9 +59,9 @@ export default function BusTicket() {
         <div class="bus-cards">
             <h2>Bus Card</h2>
             <p>$40 each</p>
-            <input type="number" id="busCards" min="0" max="5" value="0" />
+            <input type="number" id="busCards" min="0" value={busTicketNumber} onChange={(e) => {setBusTicketNumber(e?.target?.value)}}/>
         </div>
-        <button onclick="purchaseBusCards()">Purchase Bus Cards</button>
+        <button onClick={() => {purchaseBusTicket()}}>Purchase Bus Cards</button>
     </div>
     <style>
         {`
@@ -75,7 +105,7 @@ export default function BusTicket() {
             color: #fff;
             cursor: pointer;
             display: block;
-            margin: 0 auto;
+            margin: 1rem auto;
         }
         
         input {
