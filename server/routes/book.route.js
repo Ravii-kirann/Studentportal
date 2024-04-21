73c56eed096bcdad0e82a9189ea5a6538d751bcd
unknown <ravikiranjayanthi5555@gmail.com>
2024-04-21 11:35:46 -0700

    ui

--- server/routes/book.route.js
+++ server/routes/book.route.js
@@ -6,5 +6,5 @@ const verifyToken = require("../utils/verifyUser");
 router.post('/', createBook);
 router.get('/search/:key',verifyToken, searchBook);
 router.post('/textbooks/purchase',verifyToken, purchaseTextbook);
-router.get('/users/:userId/purchases', getUserPurchases);
+router.get('/users/purchases/:userId', getUserPurchases);
 module.exports = router;
